7b2c6e968b06be6de12aae39451700452e1ad60e
Deekshith D V <deekshithdv563@gmail.com>
2022-03-23 14:36:54 +0530

    Home page done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
+    before_action :authenticate_user!
     before_action :set_user, only: [:profile]
     def home
+        @posts = Post.all
     end
 
     def profile
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+    def current_user?
+        if @user.id == current_user.id
+            return true
+        else
+            return false
+        end
+    end
 end
