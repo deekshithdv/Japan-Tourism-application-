57582108f5bbfc7dd51b9014ff90bc5bb5e29924
Deekshith D V <deekshithdv563@gmail.com>
2022-03-30 15:41:04 +0530

    User profile picture featured completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :username, presence: true , uniqueness: {case_sensitive:false}
+  validates :email, presence:true , uniqueness: {case_sensitive:false}
   has_many :posts, dependent: :destroy
+  has_one_attached :avatar
 end
